059ef2206867ea0efb5f81cd49cddb334836231a
softdeveloper23 <brannongarrett77@gmail.com>
2025-11-12 17:30:38 +0100

    [Feature] Add Hello, world text and deleted Text Screen 2

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var title: String = ""
+    @State private var title: String = "Hello, world!"
     
     var body: some View {
         VStack {
             Text("Hi!")
             Text("Screen 2!")
-            Text("Screen 2!")
+
             Text("Screen 3!")
         }
         
